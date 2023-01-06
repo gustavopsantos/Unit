30b600ec82d4525b372bda728a4713b180a60e26
Gustavo Santos <gustappsantos@gmail.com>
2023-01-06 20:40:44 -0300

    Improves UDateTimePropertyDrawer

--- Assets/Unit.Editor/PropertyDrawers/UDateTimePropertyDrawer.cs
+++ Assets/Unit.Editor/PropertyDrawers/UDateTimePropertyDrawer.cs
@@ -13,27 +13,17 @@ namespace Unit.Editor.PropertyDrawers
             using (new PropertyScope(area, label, property))
             {
                 var usableArea = EditorGUI.PrefixLabel(area, GUIUtility.GetControlID(FocusType.Passive), label);
-                
-                var yearProperty = new UnitSerializedProperty(
-                    "year", property.FindPropertyRelative(nameof(UDateTime.Year)));
-                
-                var monthProperty = new UnitSerializedProperty(
-                    "month", property.FindPropertyRelative(nameof(UDateTime.Month)));
-                
-                var dayProperty = new UnitSerializedProperty(
-                    "day", property.FindPropertyRelative(nameof(UDateTime.Day)));
-                
-                var hourProperty = new UnitSerializedProperty(
-                    "hour", property.FindPropertyRelative(nameof(UDateTime.Hour)));
-                
-                var minuteProperty = new UnitSerializedProperty(
-                    "minute", property.FindPropertyRelative(nameof(UDateTime.Minute)));
-                
-                var secondProperty = new UnitSerializedProperty(
-                    "second", property.FindPropertyRelative(nameof(UDateTime.Second)));
-                
-                var kindProperty = new UnitSerializedProperty(
-                    string.Empty, property.FindPropertyRelative(nameof(UDateTime.Kind)));
+
+                var properties = new UnitSerializedProperty[]
+                {
+                    new UnitSerializedProperty("year", property.FindPropertyRelative(nameof(UDateTime.Year))),
+                    new UnitSerializedProperty("month", property.FindPropertyRelative(nameof(UDateTime.Month))),
+                    new UnitSerializedProperty("day", property.FindPropertyRelative(nameof(UDateTime.Day))),
+                    new UnitSerializedProperty("hour", property.FindPropertyRelative(nameof(UDateTime.Hour))),
+                    new UnitSerializedProperty("minute", property.FindPropertyRelative(nameof(UDateTime.Minute))),
+                    new UnitSerializedProperty("second", property.FindPropertyRelative(nameof(UDateTime.Second))),
+                    new UnitSerializedProperty(string.Empty, property.FindPropertyRelative(nameof(UDateTime.Kind))),
+                };
 
                 var cells = new Cell[]
                 {
@@ -45,18 +35,15 @@ namespace Unit.Editor.PropertyDrawers
                     new Cell(),
                     new Cell(90),
                 };
-                
-                var rects = CellFramework.GetRects(3, usableArea, cells);
-                
+
                 using (new IndentedScope(0)) // Prevents nested fields to be indented
                 {
-                    yearProperty.Present(rects[0]);
-                    monthProperty.Present(rects[1]);
-                    dayProperty.Present(rects[2]);
-                    hourProperty.Present(rects[3]);
-                    minuteProperty.Present(rects[4]);
-                    secondProperty.Present(rects[5]);
-                    kindProperty.Present(rects[6]);
+                    var rects = CellFramework.GetRects(3, usableArea, cells);
+
+                    for (var i = 0; i < properties.Length; i++)
+                    {
+                        properties[i].Present(rects[i]);
+                    }
                 }
             }
         }
