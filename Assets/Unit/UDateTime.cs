9d63d655ad96ea104b57d34f95277d140895f149
Gustavo Santos <gustappsantos@gmail.com>
2023-01-10 13:30:07 -0300

    Improves naming inside UDateTime

--- Assets/Unit/UDateTime.cs
+++ Assets/Unit/UDateTime.cs
@@ -7,32 +7,32 @@ namespace Unit
     public struct UDateTime : ISerializationCallbackReceiver
     {
         private DateTime _dateTime;
-        [SerializeField] private string _serialized; // ISO 8601 format
+        [SerializeField] private string _dateTimeString; // ISO 8601 format
 
         public UDateTime(DateTime dateTime)
         {
             _dateTime = dateTime;
-            _serialized = _dateTime.ToString("O");
+            _dateTimeString = _dateTime.ToString("O");
         }
-        
+
         public static implicit operator DateTime(UDateTime uDateTime)
         {
             return uDateTime._dateTime;
         }
-        
+
         public static implicit operator UDateTime(DateTime dateTime)
         {
             return new UDateTime(dateTime);
         }
-        
+
         public void OnBeforeSerialize()
         {
-            _serialized = _dateTime.ToString("O");
+            _dateTimeString = _dateTime.ToString("O");
         }
 
         public void OnAfterDeserialize()
         {
-            _dateTime = DateTime.Parse(_serialized, null, System.Globalization.DateTimeStyles.RoundtripKind);
+            _dateTime = DateTime.Parse(_dateTimeString, null, System.Globalization.DateTimeStyles.RoundtripKind);
         }
     }
 }
