de5cdc0f9c876d8600ad14061d1bbb4c144c46ca
Gustavo Santos <gustappsantos@gmail.com>
2023-01-11 22:24:08 -0300

    Removes unnecessary stuff

--- Assets/Unit.Editor/PropertyDrawers/UDateTimePropertyDrawer.cs
+++ Assets/Unit.Editor/PropertyDrawers/UDateTimePropertyDrawer.cs
@@ -22,24 +22,24 @@ namespace Unit.Editor.PropertyDrawers
         private DateTimeKind _kind;
 
         private Cell[] _cells;
-        private Label _yearLabel;
-        private Label _monthLabel;
-        private Label _dayLabel;
-        private Label _hourLabel;
-        private Label _minuteLabel;
-        private Label _secondLabel;
-        private Label _millisecondLabel;
+        private GUIContent _yearLabel;
+        private GUIContent _monthLabel;
+        private GUIContent _dayLabel;
+        private GUIContent _hourLabel;
+        private GUIContent _minuteLabel;
+        private GUIContent _secondLabel;
+        private GUIContent _millisecondLabel;
         private UDateTime.Precision _precision;
 
         protected override void Initialize(SerializedProperty property)
         {
-            _yearLabel = new Label("Year", "Y");
-            _monthLabel = new Label("Month", "M");
-            _dayLabel = new Label("Day", "D");
-            _hourLabel = new Label("Hour", "H");
-            _minuteLabel = new Label("Minute", "M");
-            _secondLabel = new Label("Second", "S");
-            _millisecondLabel = new Label("Millisecond", "MS");
+            _yearLabel = new GUIContent("Y");
+            _monthLabel = new GUIContent("M");
+            _dayLabel = new GUIContent("D");
+            _hourLabel = new GUIContent("H");
+            _minuteLabel = new GUIContent("M");
+            _secondLabel = new GUIContent("S");
+            _millisecondLabel = new GUIContent("MS");
 
             _precision = GetPrecision(property);
             _cells = GenerateCells(_precision);
@@ -70,8 +70,6 @@ namespace Unit.Editor.PropertyDrawers
             {
                 var usableArea = EditorGUI.PrefixLabel(area, GUIUtility.GetControlID(FocusType.Passive), label);
 
-                var compact = usableArea.width < 520;
-
                 EditorGUI.BeginChangeCheck();
                 {
                     using (new IndentedScope(0)) // Prevents nested fields to be indented
@@ -80,7 +78,7 @@ namespace Unit.Editor.PropertyDrawers
 
                         for (int i = 0; i < ((int) _precision) + 1; i++)
                         {
-                            DrawField(i, rects[i], dateTime, compact);
+                            DrawField(i, rects[i], dateTime);
                         }
                         
                         _kind = (DateTimeKind) EditorGUI.EnumPopup(rects.Last(), dateTime.Kind);
@@ -97,17 +95,17 @@ namespace Unit.Editor.PropertyDrawers
             }
         }
 
-        private void DrawField(int index, Rect rect, DateTime dateTime, bool compact)
+        private void DrawField(int index, Rect rect, DateTime dateTime)
         {
             switch (index)
             {
-                case 0: _year = GUIUtilities.IntField(_yearLabel.Get(compact), rect, dateTime.Year); break;
-                case 1: _month = GUIUtilities.IntField(_monthLabel.Get(compact), rect, dateTime.Month); break;
-                case 2: _day = GUIUtilities.IntField(_dayLabel.Get(compact), rect, dateTime.Day); break;
-                case 3: _hour = GUIUtilities.IntField(_hourLabel.Get(compact), rect, dateTime.Hour); break;
-                case 4: _minute = GUIUtilities.IntField(_minuteLabel.Get(compact), rect, dateTime.Minute); break;
-                case 5: _second = GUIUtilities.IntField(_secondLabel.Get(compact), rect, dateTime.Second); break;
-                case 6: _millisecond = GUIUtilities.IntField(_millisecondLabel.Get(compact), rect, dateTime.Millisecond); break;
+                case 0: _year = GUIUtilities.IntField(_yearLabel, rect, dateTime.Year); break;
+                case 1: _month = GUIUtilities.IntField(_monthLabel, rect, dateTime.Month); break;
+                case 2: _day = GUIUtilities.IntField(_dayLabel, rect, dateTime.Day); break;
+                case 3: _hour = GUIUtilities.IntField(_hourLabel, rect, dateTime.Hour); break;
+                case 4: _minute = GUIUtilities.IntField(_minuteLabel, rect, dateTime.Minute); break;
+                case 5: _second = GUIUtilities.IntField(_secondLabel, rect, dateTime.Second); break;
+                case 6: _millisecond = GUIUtilities.IntField(_millisecondLabel, rect, dateTime.Millisecond); break;
             }
         }
 
@@ -118,22 +116,9 @@ namespace Unit.Editor.PropertyDrawers
                 dateTime = new DateTime(_year, _month, _day, _hour, _minute, _second, _millisecond, _kind);
                 return true;
             }
-            catch (Exception e)
-            {
-                dateTime = DateTime.MinValue;
-                return false;
-            }
-        }
-
-        private bool IsValid()
-        {
-            try
-            {
-                var dt = new DateTime(_year, _month, _day, _hour, _minute, _second, _millisecond, _kind);
-                return true;
-            }
             catch (Exception)
             {
+                dateTime = DateTime.MinValue;
                 return false;
             }
         }
