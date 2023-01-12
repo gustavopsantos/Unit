d4a6c2b22c67fe4090e88583de024cdb529df49e
Gustavo Santos <gustappsantos@gmail.com>
2023-01-11 22:32:24 -0300

    Simplifies stuff

--- Assets/Unit.Editor/Utilities/GUIUtilities.cs
+++ Assets/Unit.Editor/Utilities/GUIUtilities.cs
@@ -16,14 +16,6 @@ namespace Unit.Editor.Utilities
             return newValue;
         }
         
-        public static void InnerLabeledProperty(GUIContent label, Rect area, SerializedProperty property)
-        {
-            var labelStyle = EditorStyles.label;
-            var labelRect = CalculateCellLabelRect(area, label, labelStyle);
-            EditorGUI.PropertyField(area, property, GUIContent.none);
-            EditorGUI.LabelField(labelRect, label, labelStyle);
-        }
-
         private static Rect CalculateCellLabelRect(Rect area, GUIContent label, GUIStyle style)
         {
             return area.HorizontallyTranslated(area.width - style.CalcSize(label).x - 4);
--- Assets/Unit.Sample/UnitSample.cs
+++ Assets/Unit.Sample/UnitSample.cs
@@ -4,14 +4,6 @@ namespace Unit.Sample
 {
     public class UnitSample : MonoBehaviour
     {
-        [field: SerializeField, UDateTime.Precision(UDateTime.Precision.Hour)] public UDateTime DateTime { get; private set; }
-        
-        // [SerializeField] private UDateTime[] _dateTimeArray;
-        // [SerializeField] private List<UDateTime> _dateTimeList;
-
-        // [Header("Nested")]
-        // [SerializeField] private Event _event;
-        // [SerializeField] private Event[] _eventArray;
-        // [SerializeField] private List<Event> _eventList;
+        [SerializeField] private UDateTime _dateTime;
     }
 }
